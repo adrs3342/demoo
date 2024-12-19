31424241008377af4e33241895ca0435dc072df4
ankit <a68563922@gmail.com>
2024-12-19 12:04:14 +0530

    Revert "also added multiplication with addition"
    This reverts commit 75cc2872de64aecb9e0372c5fb24b42f13bb5aa7.

--- calculator.py
+++ calculator.py
@@ -1,11 +1,5 @@
 def add(num1, num2):
     return num1 + num2
 
-def mul(num1, num2):
-    return num1 * num2
-
-def sub(num1, num2):
-    return num1 - num2
 print('addition_result:', add(5, 6))
-print('mul:')
-print('sub:')
+
